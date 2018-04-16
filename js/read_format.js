4481691bbfa59adf4168b0cd4e58f099a6d0553d
Kalle Jag Jillheden <kalle.jillheden@me.com>
2018-04-16 09:58:44 +0200

    Moved read docs to seperate file

---
+++ js/read_format.js
@@ -0,0 +1,89 @@
+
+var read = {
+    Vårdtillfällen[0..n]: {
+        Rubrik: "" String,
+        Inskrivningsdatum: Date,
+        Utskrivningsdatum: Date,
+        Diagnoser[0..n]: "" String
+        Åtgärder[0..n]: "" String
+    }
+
+    ÖppnaVårdkontakter[0..n]: {
+        Rubrik: "" String,
+        Datum: Date,
+        Diagnoser[0..n]: "" String
+        Åtgärder[0..n]: "" String
+    }
+
+    Mätvärden[0..n]: {
+        Rubrik: "" String,
+        Datum: Date,
+        RegistreradAv: "" String,
+        Värden: {
+            ["värdenamn"]: String|Number
+        }
+    }
+
+    Journaltexter[0..n]: {
+        Rubrik: "" String,
+        Datum: Date,
+        Signeringsansvarig: "" String,
+        Fritext: "" String
+    }
+
+    MikrobiologiSvar[0..n]: {
+        Rubrik: "" String,
+        Datum: Date,
+        Remittent: "" String,
+        Undersökning: "" String,
+        Provmaterial: "" String,
+        Svar: "" String
+    },
+
+    RöntgenSvar[0..n]: {
+        Rubrik: "" String,
+        Datum: Date,
+        Remittent: "" String,
+        Beställning: "" String,
+        ÖnskadUndersökning: "" String,
+        Frågeställning: "" String,
+        Svar: "" String,
+        Utlåtande: "" String
+    },
+
+    KemlabSvar[0..n] {
+        Rubrik: "" String,
+        Datum: Date,
+        Sjukhus: "" String,
+        Remittent: "" String,
+        UtanförNågotIntervall: true|false,
+        Värden[1..n]: {
+            Analysnamn: "" String,
+            Resultat: Number|String,
+            UtanförIntervall: true|false,
+            ReferensLägre: Number|null,
+            ReferensÖvre: Number|null,
+        },
+    },
+
+    MultidisciplinäraSvar[0..n] {
+        Rubrik: "" String,
+        Datum: Date,
+        Remittent: "" String,
+        UtanförNågotIntervall: true|false,
+        Värden[1..n]: {
+            Analysnamn: "" String,
+            Resultat: Number|String,
+            UtanförIntervall: true|false,
+            ReferensLägre: Number|null,
+            ReferensÖvre: Number|null,
+        },
+    },
+
+    Läkemedelsordinationer[0..n]: {
+        Rubrik: "" String,
+        Datum: Date,
+        Utsättningsdatum: Date,
+        Läkemedel[1..n]: "" String
+    }
+};
